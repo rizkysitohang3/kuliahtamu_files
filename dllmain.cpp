1f302a57f6e31052125b62112afa25ee41d05eb3
rizkysitohang3 <51305405+rizkysitohang3@users.noreply.github.com>
2021-12-01 10:51:51 +0700

    Solusi task no 3

--- dllmain.cpp
+++ dllmain.cpp
@@ -18,12 +18,43 @@ BOOL WriteFile(
 // Alamat WriteFile
 BOOL (WINAPI *True_WF)(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED) = WriteFile;
 
-// fungsi yang detour
+// fungsi detour
 BOOL WINAPI Hooked_WF(HANDLE hFile, LPCVOID lpBuffer, DWORD nOBTW, LPDWORD lpNOBW, LPOVERLAPPED lpO) {
     
     //manipulasi atau monitor disini
     MessageBoxA(HWND_DESKTOP, (LPCSTR)lpBuffer, "isi lpBuffer", MB_OK);
     return True_WF(hFile, lpBuffer, nOBTW, lpNOBW, lpO);
+}
+
+
+// Task Solution
+
+/*
+HANDLE CreateFileA(
+  [in]           LPCSTR                lpFileName,
+  [in]           DWORD                 dwDesiredAccess,
+  [in]           DWORD                 dwShareMode,
+  [in, optional] LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+  [in]           DWORD                 dwCreationDisposition,
+  [in]           DWORD                 dwFlagsAndAttributes,
+  [in, optional] HANDLE                hTemplateFile
+);
+*/
+
+// alamat CreateFile
+HANDLE (WINAPI* True_CF)(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE) = CreateFile;
+
+//fungsi detour
+HANDLE WINAPI Hooked_CF(LPCWSTR Fn, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) {
+
+    // cek parameter dwCreateDisposition
+    if (dwCreationDisposition == CREATE_NEW) {
+
+        MessageBox(HWND_DESKTOP, Fn, L"New File Created", MB_OK);
+    }
+
+
+    return True_CF(Fn, dwDesiredAccess,  dwShareMode,  lpSecurityAttributes,  dwCreationDisposition,  dwFlagsAndAttributes, hTemplateFile);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -38,6 +69,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)True_WF, Hooked_WF);
+        DetourAttach(&(PVOID&)True_CF, Hooked_CF);
 
         LONG lError = DetourTransactionCommit();
         if (lError != NO_ERROR) {
@@ -54,6 +86,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourDetach(&(PVOID&)True_WF, Hooked_WF);
+        DetourDetach(&(PVOID&)True_CF, Hooked_CF);
 
         LONG lError = DetourTransactionCommit();
         if (lError != NO_ERROR) {
